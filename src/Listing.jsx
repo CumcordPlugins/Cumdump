dfdc57da94b0e7bb36ba9c0681a75f9e9e01a9ee
Cain Atkinson <yellowsink@protonmail.com>
2021-10-24 00:51:58 +0100

    switch to fetch oh my god

--- src/Listing.jsx
+++ src/Listing.jsx
@@ -1,33 +1,34 @@
+import { createSignal, createResource } from "solid-js";
 import PluginCard from "./PluginCard.jsx";
 
 const repo = "https://cumcordplugins.github.io/Condom/plugins-large.json";
 
-export default () => {
-  // ik that xmlhttprequest, especially non-async, is awful,
-  // but i just cant deal with async solid components today
-  const xhr = new XMLHttpRequest();
-  xhr.open("GET", repo, false);
-  xhr.send();
-  const pluginsRaw = JSON.parse(xhr.responseText);
+async function getPlugins() {
+  const pluginsRaw = await (await fetch(repo)).json();
   const plugins = Object.keys(pluginsRaw).map((key) => {
     let plugin = pluginsRaw[key];
     plugin.url = key;
     return plugin;
   });
+  return plugins;
+}
 
-  console.log(plugins);
+export default () => {
+  const [signal, sigsend] = createSignal();
+  const [plugins] = createResource(signal, getPlugins);
+  sigsend("");
 
   return (
-      <div class="mx-20">
-        <h2 class="text-xl text-text-secondary mb-4">all plugins</h2>
+    <div class="mx-20">
+      <h2 class="text-xl text-text-secondary mb-4">all plugins</h2>
 
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 mb-4">
-          {plugins.map((plugin) => (
-            <PluginCard plugin={plugin} />
-          ))}
-        </div>
-
-        <iframe class="hidden" id="teledildonic" />
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 mb-4">
+        {plugins.loading && "Loading plugins..."}
+        {plugins.error ?? []}
+        {plugins()?.map((plugin) => <PluginCard plugin={plugin} />) ?? []}
       </div>
+
+      <iframe class="hidden" id="teledildonic" />
+    </div>
   );
 };
