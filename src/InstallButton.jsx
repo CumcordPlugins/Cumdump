00a9f055a87dcfde2cc9d4cf23335aea363f133b
Cain Atkinson <yellowsink@protonmail.com>
2021-10-27 08:14:50 +0100

    stuff for web users

--- src/InstallButton.jsx
+++ src/InstallButton.jsx
@@ -1,7 +1,15 @@
-export default ({ url }) => (
+export default ({ url, hash }) => (
   <button
     class="bg-blurple py-1 px-6"
     onClick={() => {
+      let css = document.getElementById(`tempcss_${hash}`);
+      css.innerText = `#overlay_${hash} { opacity: 1 !important; }`;
+      setTimeout(() => {
+        css.innerText = "";
+      }, 2000);
+
+      navigator.clipboard.writeText(url);
+
       let iframe = document.getElementById("teledildonic");
       // this can be literally anything, gets around not reloading due to only difference being #
       iframe.src = "cum";
@@ -11,5 +19,6 @@ export default ({ url }) => (
     }}
   >
     INSTALL
+    <style id={`tempcss_${hash}`} />
   </button>
 );
--- src/PluginCard.jsx
+++ src/PluginCard.jsx
@@ -1,16 +1,28 @@
 import InstallButton from "./InstallButton.jsx";
 
 export default ({ plugin }) => (
-  <div class="bg-cardbg border-black border shadow-md p-2 min-h-38 flex flex-col">
-    <div class="text-lg">{plugin.name}</div>
-    <div class="text-sm">{plugin.description}</div>
+  <div class="bg-cardbg border-black border shadow-md p-2 min-h-38 grid">
+    {/* card to show over the top when installed */}
+    <div id={`overlay_${plugin.hash}`} class="transition-all opacity-0 pointer-events-none row-start-1 col-start-1 z-1 -m-2 bg-gradient-to-t from-gradt to-gradb">
+      <h2 class="text-2xl mt-4 text-center">Using Discord Web?</h2>
+      <p class="mt-2 text-center">
+        I've copied the link to your clipboard. Paste it into the Cumcord
+        Plugins panel in settings.
+      </p>
+    </div>
 
-    <div class="flex-1" />
+    {/* card contents */}
+    <div class="flex flex-col row-start-1 col-start-1">
+      <div class="text-lg">{plugin.name}</div>
+      <div class="text-sm">{plugin.description}</div>
 
-    <div class="bg-cardbgalt h-12 -m-2 mt-4 text-sm flex items-center p-2">
+      <div class="flex-1" />
+
+      <div class="bg-cardbgalt h-12 -m-2 mt-4 text-sm flex items-center p-2">
         {plugin.author}
         <div class="flex-1" />
-        <InstallButton url={plugin.url} />
+        <InstallButton url={plugin.url} hash={plugin.hash} />
+      </div>
     </div>
   </div>
 );
