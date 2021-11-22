aaba552f356dcf2c1da3f9f3aaff0b163eb0acd1
Cain Atkinson <yellowsink@protonmail.com>
2021-11-22 21:41:17 +0000

    show newest plugins first

--- src/repoFetcher.js
+++ src/repoFetcher.js
@@ -1,3 +1,3 @@
 const repo = "https://cumcordplugins.github.io/Condom/plugins-large.json";
 
-export default async () => await (await fetch(repo)).json();
+export default async () => (await (await fetch(repo)).json()).reverse();
