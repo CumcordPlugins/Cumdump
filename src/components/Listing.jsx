e9ed4018bb48a1f4e0d9663f5d665d876c572869
creatable <creatableog@protonmail.com>
2021-11-25 07:58:13 -0500

    Use Windi breakpoint groups

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -23,7 +23,7 @@ export default () => {
         />
       </div>
 
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4 md:mx-20 mx-4">
+      <div class="grid grid-cols-1 md:(grid-cols-2 mx-20) lg:grid-cols-3 2xl:grid-cols-4 gap-4 mx-4">
         {plugins.loading && "Loading plugins..."}
         {plugins.error ?? []}
         {fuzzy(plugins(), search())?.map((plugin) => (
